083c197fb40b7a29241f525a9c2c5b0800d3476c
Daniel Levi <daniellevi000@gmail.com>
2023-03-26 10:20:07 -0300

    "first Comit"

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+import logo from '../assets/Logo.svg'
+import Image from 'next/legacy/image'
+import { Container, Header } from "../styles/pages/app";
+
+globalStyles();
+export default function App({ Component, pageProps }: AppProps) {
+  return(
+    <Container>
+      <Header>
+        <Image src={logo} alt="" />
+      </Header>
+       <Component {...pageProps} />
+    </Container>
+   
+  ) 
+}
